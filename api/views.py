89016d07eff15be88ef08626b456b9169b827706
Joseph Chambers <joseph@michael-chambers.com>
2023-01-04 20:13:43 -0500

    model routes

--- api/views.py
+++ api/views.py
@@ -3,4 +3,25 @@ from django.http import JsonResponse
 
 # Create your views here.
 def getRoutes(request):
-    return JsonResponse('API Routes', safe=False)
+    routes = [
+        {
+            'Endpoint': '/questions/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of questions'
+        },
+        {
+            'Endpoint': '/questions/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single question object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new question with data sent in post request'
+        }
+    ]
+
+    return JsonResponse(routes, safe=False)
